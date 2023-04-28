6fe545b71c165c0dcd604efa6acae7cb6dc1ea38
anthonybartczak <to0oni456@gmail.com>
2023-04-28 15:18:56 +0200

    Added allowed hosts

--- django_orbat_creator/settings.py
+++ django_orbat_creator/settings.py
@@ -37,9 +37,9 @@ SECRET_KEY = 'django-insecure-in8a##!mhc6$)8)xav)w3e8r=cvb+w#je*c&uz9%#vnjjibd*q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'orbat.toadres.pl']
+ALLOWED_HOSTS = ['http://localhost:3000/', '127.0.0.1', 'orbat.toadres.pl']
 
-CSRF_TRUSTED_ORIGINS = ['http://0.0.0.0:8000', 'https://orbat.toadres.pl']
+CSRF_TRUSTED_ORIGINS = ['http://localhost:3000/', 'http://0.0.0.0:8000', 'https://orbat.toadres.pl']
 
 # Application definition
 
